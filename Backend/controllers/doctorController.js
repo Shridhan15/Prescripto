fdca7a4802fce1d1d12492db33d33a4344c5d758
Shridhan <shridhan.suman2023@vitstudent.ac.in>
2025-04-23 17:31:54 +0530

    finished doctor appointments

--- Backend/controllers/doctorController.js
+++ Backend/controllers/doctorController.js
@@ -57,15 +57,49 @@ const loginDoctor = async (req, res) => {
 }
 
 // api to get doctor appointments for doctor pannel
-const appointmentsDoctor= async(req,res)=>{
+const appointmentsDoctor = async (req, res) => {
     try {
-        const {docId}=req.body
-        const appointments= await appointmentModel.find({docId})
-        res.json({success:true,appointments})
+        const { docId } = req.body
+        const appointments = await appointmentModel.find({ docId })
+        res.json({ success: true, appointments })
 
     } catch (error) {
         console.log(error)
         res.json({ success: false, message: error.message })
     }
 }
-export { changeAvailability, doctorList, loginDoctor,appointmentsDoctor }
+
+//api to mark appointment completed for doctor pannel
+const appointmentComplete = async (req, res) => {
+    try {
+        const { docId, appointmentId } = req.body //appointmentId will be passed
+        const appointmentData = await appointmentModel.findById(appointmentId)
+        if (appointmentData && appointmentData.docId === docId) {
+            await appointmentModel.findByIdAndUpdate(appointmentId, { isCompleted: true })
+            return res.json({ success: true, message: "Appointment Completed" })
+        } else {
+            return res.json({ success: false, message: "Mark Failed" })
+        }
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+//api to cancel appointment completed for doctor pannel
+const appointmentCancel = async (req, res) => {
+    try {
+        const { docId, appointmentId } = req.body //appointmentId will be passed
+        const appointmentData = await appointmentModel.findById(appointmentId)
+        if (appointmentData && appointmentData.docId === docId) {
+            await appointmentModel.findByIdAndUpdate(appointmentId, { cancelled: true })
+            return res.json({ success: true, message: "Appointment Cancelled" })
+        } else {
+            return res.json({ success: false, message: "Cancellation Failed" })
+        }
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+export { changeAvailability, doctorList, loginDoctor, appointmentsDoctor, appointmentCancel, appointmentComplete }
--- admin/src/pages/Doctor/DoctorAppointment.jsx
+++ admin/src/pages/Doctor/DoctorAppointment.jsx
@@ -1,8 +1,18 @@
 import React, { useContext, useEffect } from "react";
 import { DoctorContext } from "../../context/DoctorContext";
+import { AppContext } from "../../context/AppContext";
+import { assets } from "../../assets/assets";
 
 const DoctorAppointment = () => {
-  const { dToken, appointments, getAppointments } = useContext(DoctorContext);
+  const {
+    dToken,
+    appointments,
+    getAppointments,
+    completeAppointment,
+    cancelAppointment,
+  } = useContext(DoctorContext);
+  const { calculateAge, slotDateFormat, currency } = useContext(AppContext);
+
   useEffect(() => {
     if (dToken) {
       getAppointments();
@@ -21,6 +31,56 @@ const DoctorAppointment = () => {
           <p>Fees</p>
           <p>Action</p>
         </div>
+
+        {appointments.reverse().map((item, index) => (
+          <div
+            className="flex flex-wrap justify-between max-sm:gap-5 max-sm:text-base sm:grid grid-cols-[0.5fr_2fr_1fr_1fr_3fr_1fr_1fr] gap-1 items-center text-gray-500 py-3 px-6   border-b hover:bg-gray-100"
+            key={index}
+          >
+            <p className="max-sm:hidden">{index + 1} </p>
+            <div className="flex items-center gap-2">
+              <img
+                className="w-8 rounded-full"
+                src={item.userData.image}
+                alt=""
+              />
+              <p>{item.userData.name} </p>
+            </div>
+            <div>
+              <p className="text-xs inline border border-primary px-2 rounded-full">
+                {item.payment ? "Online" : "CASH"}
+              </p>
+            </div>
+            <p className="max-sm:hidden"> {calculateAge(item.userData.dob)} </p>
+            <p>
+              {slotDateFormat(item.slotDate)}, {item.slotTime}{" "}
+            </p>
+            <p>
+              {currency}
+              {item.amount}
+            </p>
+            {item.cancelled ? (
+              <p className="text-red-400 text-xs font-medium">Cancelled</p>
+            ) : item.isCompleted ? (
+              <p className=" text-green-500 text-xs font-medium"> Completed </p>
+            ) : (
+              <div className="flex">
+                <img
+                  onClick={() => cancelAppointment(item._id)}
+                  className="w-10 cursor-pointer"
+                  src={assets.cancel_icon}
+                  alt=""
+                />
+                <img
+                  onClick={() => completeAppointment(item._id)}
+                  className="w-10 cursor-pointer"
+                  src={assets.tick_icon}
+                  alt=""
+                />
+              </div>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
