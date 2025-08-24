d668a183a41dba0aba89f1f0ea728ff7fb434737
Shridhan <shridhan.suman2023@vitstudent.ac.in>
2025-08-24 15:47:59 +0530

    changed cors

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,17 @@ connectCloudinary()
 
 //middlewares
 app.use(express.json())
-app.use(cors()) // allow connecting frontend and backend
+// app.use(cors()) // allow connecting frontend and backend
+app.use(cors({
+  origin: ["https://prescripto-frontend-29f1.onrender.com/"],  // your deployed frontend
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "token"],
+  credentials: true
+}));
+
+// ensure OPTIONS requests handled
+app.options("*", cors());
+
 
 // api end point for admin doctor and user
 app.use("/api/admin",adminRouter)//localhost/4000/api/admin/add-doctor
